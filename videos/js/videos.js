2ed1a7dde88611ec17c41e573850dc733dcf5c45
Arely Paulina Rojas Esteban <37195469+ArelyStevan@users.noreply.github.com>
2020-02-23 21:00:47 -0600

    Add files via upload

---
+++ videos/js/videos.js
@@ -0,0 +1,35 @@
+//variables globales
+var position=0;
+var playlist;
+var video;
+
+window.onload = function(){
+	playlist= ["video/v1", "video/v2","video/v3","video/v4","video/v5","video/v6","video/v7"];
+	video = document.getElementById ('video');
+	video.addEventListener("ended", nextVideo, false);
+
+	video.src = playlist[position] + getFormatExtension();
+	video.load();
+	video.play(); 
+};
+
+function nextVideo(){
+	position ++;
+	if(position >= playlist.length){
+		position=0;
+	}
+
+	video.src = playlist[position] + getFormatExtension();
+	video.load();
+	video.play(); 
+}
+
+function getFormatExtension(){
+	if (video.canPlayType("video/mp4") !="") {
+		return ".mp4";
+	} else if (video.canPlayType("video/ogg") != ""){
+		return ".ogv";
+	} else if (video.canPlayType("video/webm") != ""){
+		return ".webm";
+	}
+}
